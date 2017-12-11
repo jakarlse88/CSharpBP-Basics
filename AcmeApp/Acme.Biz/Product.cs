f2b7c658dee93ec713e059767fcb1fc1daa5f330
Jon Karlsen <karlsen.jonarild@gmail.com>
2017-12-11 11:17:07 +0100

    Begin module "Creating Good Properties"

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Acme.Common;
+using static Acme.Common.LoggingService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +13,14 @@ namespace Acme.Biz
 	/// </summary>
 	public class Product
 	{
+		public const double InchesPerMeter = 39.37;
+		public readonly decimal MinimumPrice;
+
+		#region Constructors
 		public Product()
 		{
+			//this.ProductVendor = new Vendor();
+			this.MinimumPrice = .96m;
 			Console.WriteLine("Product instance created.");
 		}
 
@@ -23,10 +31,16 @@ namespace Acme.Biz
 			this.ProductId = productId;
 			this.ProductName = productName;
 			this.Description = description;
+			if (this.ProductName.StartsWith("Bulk"))
+			{
+				this.MinimumPrice = 9.99m;
+			}
 
 			Console.WriteLine("Product instance has a name: " + ProductName);
 		}
+		#endregion
 
+		#region Properties
 		private string productName;
 
 		public string ProductName
@@ -50,12 +64,53 @@ namespace Acme.Biz
 			get { return productId; }
 			set { productId = value; }
 		}
-	
+
+		private Vendor productVendor;
+
+		public Vendor ProductVendor
+		{
+			get
+			{
+				if (this.productVendor == null)
+				{
+					productVendor = new Vendor();
+				}
+				return productVendor;
+			}
+			set { productVendor = value; }
+		}
+
+		private DateTime? availabilityDate;
+
+		public DateTime? AvailabilityDate
+		{
+			get { return availabilityDate; }
+			set { availabilityDate = value; }
+		}
+
+		#endregion
+
+		#region Methods
 		public string SayHello()
 		{
+			//var vendor = new Vendor();
+			//vendor.SendWelcomeEmail("Message from Product");
+			
+
+			var emailService = new EmailService();
+			var confirmation = emailService.SendMessage("New Product",
+														this.productName,
+														"sales@abc.com");
+
+			var result = LogAction("Saying hello");
+
 			return "Hello " + ProductName +
 					" (" + ProductId + "): " +
-					Description;
+					Description +
+					" Available on: " +
+					AvailabilityDate?.ToShortDateString();
 		}
+
+		#endregion
 	}
 }
--- AcmeApp/Tests/Acme.BizTests/ProductTests.cs
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -19,8 +19,10 @@ namespace Acme.Biz.Tests
 			currentProduct.ProductName = "Saw";
 			currentProduct.ProductId = 1;
 			currentProduct.Description = "15-inch steel blade hand saw";
+			currentProduct.ProductVendor.CompanyName = "ABC Corp.";
 
-			var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+			var expected = "Hello Saw (1): 15-inch steel blade hand saw" +
+							" Available on: ";
 
 			//-- Act
 			var actual = currentProduct.SayHello();
@@ -36,10 +38,89 @@ namespace Acme.Biz.Tests
 			var currentProduct = new Product(1, "Saw", 
 			"15-inch steel blade hand saw");
 
-			var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+			var expected = "Hello Saw (1): 15-inch steel blade hand saw" +
+							" Available on: ";
 
 			//-- Act
 			var actual = currentProduct.SayHello();
+
+			//-- Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void SayHelloTest_ObjectInitialiser()
+		{
+			//-- Arrange
+			var currentProduct = new Product
+			{
+				ProductId = 1,
+				ProductName = "Saw",
+				Description = "15-inch steel blade hand saw"
+			};
+
+			var expected = "Hello Saw (1): 15-inch steel blade hand saw" +
+							" Available on: ";
+
+			//-- Act
+			var actual = currentProduct.SayHello();
+
+			//-- Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Product_Null()
+		{
+			//-- Arrange
+			Product currentProduct = null;
+			var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+			string expected = null;
+
+			//-- Act
+			var actual = companyName;
+
+			//-- Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void ConvertMetersToInchesTest()
+		{
+			//-- Arrange
+			var expected = 78.74;
+
+			//-- Act
+			var actual = 2 * Product.InchesPerMeter;
+
+			//-- Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void MinimumPriceTest_Default()
+		{
+			//-- Arrange
+			var currentProduct = new Product();
+			var expected = .96m;
+
+			//-- Act
+			var actual = currentProduct.MinimumPrice;
+
+			//-- Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void MinimumPriceTest_Bulk()
+		{
+			//-- Arrange
+			var currentProduct = new Product(1, "Bulk Tools", "");
+			var expected = 9.99m;
+
+			//-- Act
+			var actual = currentProduct.MinimumPrice;
 
 			//-- Assert
 			Assert.AreEqual(expected, actual);
